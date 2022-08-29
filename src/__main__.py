779c4d8c6075eda5a7d5f01d6da14793952536df
Sam <46686300+suchasaltylemon@users.noreply.github.com>
2022-08-29 14:14:43 +0100

    Fixed loading of Crypto lib

--- src/__main__.py
+++ src/__main__.py
@@ -1,64 +1,69 @@
 from signalio import Signal, SecureClient, SecureServer, SecureConnection
 
-s = SecureServer("0.0.0.0", 7777)
+do_server = input("Server? ") == "y"
 
-usernames = {}
+if do_server:
+    s = SecureServer("0.0.0.0", 7777)
+
+    usernames = {}
 
 
-@s.Connected()
-def h(conn: SecureConnection):
-    @conn.Signalled("/username/set")
-    def set_username(signal):
-        username = signal.data["username"]
-        usernames[conn] = username
+    @s.Connected()
+    def h(conn: SecureConnection):
+        @conn.Signalled("/username/set")
+        def set_username(signal):
+            username = signal.data["username"]
+            usernames[conn] = username
 
-        s.send_to_all(Signal("/message/echo", {
-            "author": "SERVER",
-            "message": f"{username} has joined"
+            s.send_to_all(Signal("/message/echo", {
+                "author": "SERVER",
+                "message": f"{username} has joined"
+            }))
+
+        @conn.Signalled("/message")
+        def handle_message(signal):
+            username = usernames[conn]
+            message = signal.data["message"]
+
+            s.send_to_all(Signal("/message/echo", {
+                "message": message,
+                "author": username
+            }))
+
+
+    s.start()
+
+else:
+
+    c = SecureClient("127.0.0.1", 7777)
+
+
+    @c.Connected()
+    def handle_connection(conn):
+        if not conn.secure:
+            conn.Secured.wait()
+
+        print("Connected")
+
+        username = input("Enter username: ")
+
+        conn.send(Signal("/username/set", {
+            "username": username
         }))
 
-    @conn.Signalled("/message")
-    def handle_message(signal):
-        username = usernames[conn]
-        message = signal.data["message"]
+        @conn.Signalled("/message/echo")
+        def echo_message(signal):
+            recv_message = signal.data["message"]
+            author = signal.data["author"]
 
-        s.send_to_all(Signal("/message/echo", {
-            "message": message,
-            "author": username
-        }))
+            print(f"{author}) {recv_message}")
 
+        while True:
+            send_message = input(" > ")
 
-s.start()
-
-c = SecureClient("127.0.0.1", 7777)
-
-
-@c.Connected()
-def handle_connection(conn):
-    if not conn.secure:
-        conn.Secured.wait()
-
-    print("Connected")
-
-    username = input("Enter username: ")
-
-    conn.send(Signal("/username/set", {
-        "username": username
-    }))
-
-    @conn.Signalled("/message/echo")
-    def echo_message(signal):
-        recv_message = signal.data["message"]
-        author = signal.data["author"]
-
-        print(f"{author}) {recv_message}")
-
-    while True:
-        send_message = input(" > ")
-
-        conn.send(Signal("/message", {
-            "message": send_message
-        }))
+            conn.send(Signal("/message", {
+                "message": send_message
+            }))
 
 
-c.connect()
+    c.connect()
